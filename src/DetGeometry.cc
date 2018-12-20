297d36f44b73ff0ae2bc83086e9421adb40422a9
EliteUser <unknownuser017@gmail.com>
2018-12-20 15:23:24 +0300

    qq

--- src/DetGeometry.cc
+++ src/DetGeometry.cc
@@ -39,15 +39,15 @@ G4VPhysicalVolume* DetGeometry::Construct(){
     fout<<polystyrene<<'\n';
     fout<<YAP<<'\n';
 
-    G4Tubs *scin_CH = new G4Tubs("scin_CH", 0, 2.5*cm, 5*cm, 90*deg, 360*deg);
+    G4Tubs *scin_CH = new G4Tubs("scin_CH", 0, 2.5*cm, 5*cm, 0, 360*deg);
     G4LogicalVolume *scin_CH_log = new G4LogicalVolume(scin_CH, polystyrene, "scin_CH_log");
     scin_CH_log->SetVisAttributes(G4Colour::Green());
-    new G4PVPlacement(new G4RotationMatrix(90*deg,90*deg,0),G4ThreeVector(140*cm,0,0),scin_CH_log,"scin_CH_PVP",logicWorld,false,0);
+    new G4PVPlacement(new G4RotationMatrix(90*deg,90*deg,0),G4ThreeVector(10*cm,0,0),scin_CH_log,"scin_CH_PVP",logicWorld,false,0);
 
-    G4Tubs *scin_YAP = new G4Tubs("scin_YAP", 0, 1.25*cm, 1.25*cm, 90*deg, 360*deg);
+    G4Tubs *scin_YAP = new G4Tubs("scin_YAP", 0, 1.25*cm, 1.25*cm, 0, 360*deg);
     G4LogicalVolume *scin_YAP_log = new G4LogicalVolume(scin_YAP, YAP, "scin_YAP_log");
     scin_YAP_log->SetVisAttributes(G4Colour::Red());
-    new G4PVPlacement(new G4RotationMatrix(90*deg,90*deg,0),G4ThreeVector(-140*cm,0,0),scin_YAP_log,"scin_YAP_PVP",logicWorld,false,0);
+    new G4PVPlacement(new G4RotationMatrix(90*deg,90*deg,0),G4ThreeVector(-10*cm,0,0),scin_YAP_log,"scin_YAP_PVP",logicWorld,false,0);
 
     return physWorld;
 }
--- src/PrimaryGen.cc
+++ src/PrimaryGen.cc
@@ -4,7 +4,7 @@
 #include "PrimaryGen.hh"
 
 PrimaryGen::PrimaryGen() {
-    gun = new G4ParticleGun(100);
+    gun = new G4ParticleGun(1000);
     G4cout<<"Primary particles generator is created successfully\t\tOK!!!"<<G4endl;
 }
 
@@ -16,16 +16,6 @@ void PrimaryGen::GeneratePrimaries(G4Event* anEvent) {
     gun->SetParticleDefinition(G4Gamma::GammaDefinition());
     gun->SetParticleEnergy(662 * keV);
 
-//    double R = 10*cm, px, py, pz;
-//
-//     do {
-//        px = (2*G4UniformRand()-1) * cm;
-//        py = (2*G4UniformRand()-1) * cm;
-//        pz = (2*G4UniformRand()-1) * cm;
-//    } while (sqrt(px*px + py*py + pz*pz) > R);
-//
-//    gun->SetParticlePosition(G4ThreeVector(px,-3*m + py, pz));
-
     gun->SetParticlePosition(G4ThreeVector(0,0,0));
 
     double x = 2*G4UniformRand()-1;
@@ -33,9 +23,7 @@ void PrimaryGen::GeneratePrimaries(G4Event* anEvent) {
     double z = 2*G4UniformRand()-1;
     double r = sqrt(x*x + y*y + z*z);
 
-//    gun->SetParticleMomentumDirection(G4ThreeVector(x/r, y/r, z/r));
-    gun->SetParticleMomentumDirection(G4ThreeVector(-1, 0, 0));
-
+    gun->SetParticleMomentumDirection(G4ThreeVector(x/r, y/r, z/r));
 
     gun->GeneratePrimaryVertex(anEvent);
 }
